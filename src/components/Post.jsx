820a14f1184b999b1c54eeef52784b827613be2d
Caiofrt9 <silvacaio295@gmail.com>
2023-04-07 10:25:26 -0300

    post comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,9 +3,16 @@ import ptBR from 'date-fns/locale/pt-BR'
 
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
+import { useState } from 'react'
 
 export function Post({author, publishedAt, content}) {
+
+  const [comments, setComments] = useState([ 
+    1,
+    2,
+  ])
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'",{
     locale: ptBR
@@ -16,6 +23,14 @@ export function Post({author, publishedAt, content}) {
     addSuffix: true,
 
   }) 
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1])
+
+   
+  }
   
    return (
     <article className={styles.post}>
@@ -42,7 +57,7 @@ export function Post({author, publishedAt, content}) {
           }
         })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea
           placeholder="Deixe seu comentário"
@@ -53,7 +68,9 @@ export function Post({author, publishedAt, content}) {
       </form> 
 
       <div className={styles.commentList}>
-        <Comment/>
+        {comments.map(comment => {
+          return <Comment/>
+        })}
       </div>
     </article>
   )
