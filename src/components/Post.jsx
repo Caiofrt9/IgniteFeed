d2b164a534844119c246ebaaea2be196b9bf3c3a
Caiofrt9 <silvacaio295@gmail.com>
2023-04-25 12:07:12 -0300

    comment validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,7 +33,12 @@ export function Post({author, publishedAt, content}) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+ 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório')
   }
 
   function deleteComment(commentToDelete) {
@@ -44,6 +49,8 @@ export function Post({author, publishedAt, content}) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length == 0
   
    return (
     <article className={styles.post}>
@@ -77,9 +84,13 @@ export function Post({author, publishedAt, content}) {
           placeholder="Deixe seu comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type='submit'>Públicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Públicar
+          </button>
         </footer>
       </form> 
 
